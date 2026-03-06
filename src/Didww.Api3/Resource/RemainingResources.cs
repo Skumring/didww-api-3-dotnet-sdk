f1af17d363c7c748a89902affec2d708f2ff3469
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-03-06 23:02:14 +0100

    Complete Requirement resource attributes and relationships
    Add all missing attributes matching Java/PHP/Python/Go SDKs:
    personalAreaLevel, businessAreaLevel, addressAreaLevel,
    personalProofQty, businessProofQty, addressProofQty,
    personalMandatoryFields, businessMandatoryFields,
    serviceDescriptionRequired, restrictionMessage.
    
    Add all missing relationships: country, didGroupType,
    personalPermanentDocument, businessPermanentDocument,
    personalOnetimeDocument, businessOnetimeDocument,
    personalProofTypes, businessProofTypes, addressProofTypes.

--- src/Didww.Api3/Resource/RemainingResources.cs
+++ src/Didww.Api3/Resource/RemainingResources.cs
@@ -122,12 +122,27 @@ public class Requirement : BaseResource
 {
     public override string Type => "requirements";
 
-    [JsonProperty("description")] public string? Description { get; set; }
     [JsonProperty("identity_type")] public IdentityType? IdentityType { get; set; }
-    [JsonProperty("area_level")] public AreaLevel? AreaLevel { get; set; }
+    [JsonProperty("personal_area_level")] public AreaLevel? PersonalAreaLevel { get; set; }
+    [JsonProperty("business_area_level")] public AreaLevel? BusinessAreaLevel { get; set; }
+    [JsonProperty("address_area_level")] public AreaLevel? AddressAreaLevel { get; set; }
+    [JsonProperty("personal_proof_qty")] public int? PersonalProofQty { get; set; }
+    [JsonProperty("business_proof_qty")] public int? BusinessProofQty { get; set; }
+    [JsonProperty("address_proof_qty")] public int? AddressProofQty { get; set; }
+    [JsonProperty("personal_mandatory_fields")] public List<string>? PersonalMandatoryFields { get; set; }
+    [JsonProperty("business_mandatory_fields")] public List<string>? BusinessMandatoryFields { get; set; }
+    [JsonProperty("service_description_required")] public bool? ServiceDescriptionRequired { get; set; }
+    [JsonProperty("restriction_message")] public string? RestrictionMessage { get; set; }
 
-    [JsonProperty("proof_types")] public List<ProofType>? ProofTypes { get; set; }
-    [JsonProperty("supporting_document_templates")] public List<SupportingDocumentTemplate>? SupportingDocumentTemplates { get; set; }
+    [JsonProperty("country")] public Country? Country { get; set; }
+    [JsonProperty("did_group_type")] public DidGroupType? DidGroupType { get; set; }
+    [JsonProperty("personal_permanent_document")] public SupportingDocumentTemplate? PersonalPermanentDocument { get; set; }
+    [JsonProperty("business_permanent_document")] public SupportingDocumentTemplate? BusinessPermanentDocument { get; set; }
+    [JsonProperty("personal_onetime_document")] public SupportingDocumentTemplate? PersonalOnetimeDocument { get; set; }
+    [JsonProperty("business_onetime_document")] public SupportingDocumentTemplate? BusinessOnetimeDocument { get; set; }
+    [JsonProperty("personal_proof_types")] public List<ProofType>? PersonalProofTypes { get; set; }
+    [JsonProperty("business_proof_types")] public List<ProofType>? BusinessProofTypes { get; set; }
+    [JsonProperty("address_proof_types")] public List<ProofType>? AddressProofTypes { get; set; }
 }
 
 public class RequirementValidation : BaseResource
